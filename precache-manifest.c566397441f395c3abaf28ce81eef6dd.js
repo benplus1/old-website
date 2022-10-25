4e58c561abeaf3cd7d3d42689c362c184a4e26fa
benplus1 <yang.benjamin1998@gmail.com>
2022-10-25 18:14:09 -0400

    first commit

---
+++ precache-manifest.c566397441f395c3abaf28ce81eef6dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11a4fdf0b75ce3413afa4745af7b2eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b8ff4c2f305d22b85e",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b8ff4c2f305d22b85e",
+    "url": "/static/js/main.cf1b7b94.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6175d14e16a9e34fe2d8b50c2fb67930",
+    "url": "/static/media/benyang.6175d14e.png"
+  },
+  {
+    "revision": "6c3eb3623da691a570acc19883f214f0",
+    "url": "/static/media/resume.6c3eb362.pdf"
+  }
+]);
